148c09e82420f0f1afa5e22a91691ba173ecd6cb
GaboSO21 <gso2090@gmail.com>
2023-02-13 09:24:32 -0800

    Creacion de Proyecto

---
+++ salvus/src/main/java/com/proyecto/salvus/SalvusApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.salvus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalvusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvusApplication.class, args);
+	}
+
+}
---
+++ salvus/src/test/java/com/proyecto/salvus/SalvusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.salvus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalvusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
