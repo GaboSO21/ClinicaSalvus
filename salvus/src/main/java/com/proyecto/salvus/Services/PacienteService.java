80a83db05feefcab1493ded1063cebf58a9b0106
GaboSO21 <gso2090@gmail.com>
2023-03-09 21:27:18 -0600

    Paciente controller

--- salvus/src/main/java/com/proyecto/salvus/Services/PacienteService.java
+++ salvus/src/main/java/com/proyecto/salvus/Services/PacienteService.java
@@ -2,7 +2,34 @@ package com.proyecto.salvus.Services;
 
 import org.springframework.stereotype.Service;
 
+import com.proyecto.salvus.Models.Paciente;
+import com.proyecto.salvus.Repositories.PacienteRepository;
+
+import java.util.*;
+
 @Service
 public class PacienteService {
+
+    private PacienteRepository pacienteRepository;
+
+    public PacienteService(PacienteService pacienteService) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    public List<Paciente> selectPacientes() {
+        return (List<Paciente>) pacienteRepository.findAll();
+    }
+
+    public Paciente getServicio(Paciente paciente) {
+        return pacienteRepository.findById(paciente.getCedula()).orElse(null);
+    }
+
+    public void deletePaciente(Paciente paciente) {
+        pacienteRepository.delete(paciente);
+    }
+
+    public void saveServicio(Paciente paciente) {
+        pacienteRepository.save(paciente);
+    }
 
 }
